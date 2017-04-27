ab5c8f7b66a547e228ae558daee3be860ab00693
Felix Milea-Ciobanu <fmileaciobanu@instructure.com>
2017-04-27 11:30:03 -0600

    register ui

---
+++ client/app/bundles/HelloWorld/startup/TeacherRegisterApp.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+import ApplyTheme from 'instructure-ui/lib/components/ApplyTheme'
+import 'instructure-ui/lib/themes/canvas'
+ApplyTheme.setDefaultTheme('canvas')
+
+import Typography from 'instructure-ui/lib/components/Typography'
+import Heading from 'instructure-ui/lib/components/Heading'
+import FormFieldGroup from 'instructure-ui/lib/components/FormFieldGroup'
+import TextInput from 'instructure-ui/lib/components/TextInput'
+import Button from 'instructure-ui/lib/components/Button'
+
+
+// Canvas Url: <input id="canvasDomain" value="https://karl.instructure.com" />
+// <button onclick="authenticate()">Authenticate</button>
+
+
+class TeacherRegisterApp extends Component {
+  componentDidMount () {
+    microsoftTeams.initialize()
+  }
+
+  authenticate () {
+    microsoftTeams.authentication.authenticate({
+      url: '/auth?canvas_host=' + this.urlInput.value,
+      width: '1000',
+      height: '1000',
+      successCallback: () => { window.location.replace('/teams/register_class') },
+      failureCallback: () => { alert('failure :(') },
+    })
+  }
+
+  onRegisterClick = () => {
+    this.authenticate()
+  }
+
+  render () {
+    return (
+      <div>
+        <FormFieldGroup description=" " label=" ">
+          <Heading level="h1" color="primary-inverse">Register Canvas</Heading>
+          <TextInput
+            ref={(c) => { this.urlInput = c }}
+            label={<Typography color="primary-inverse">Canvas URL:</Typography>}
+            defaultValue="https://karl.instructure.com"
+          />
+          <Button variant="primary" onClick={this.onRegisterClick}>Register</Button>
+        </FormFieldGroup>
+      </div>
+    )
+  }
+}
+
+export default TeacherRegisterApp
--- client/app/bundles/HelloWorld/startup/registration.jsx
+++ client/app/bundles/HelloWorld/startup/registration.jsx
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorldApp from './HelloWorldApp';
+import TeacherRegisterApp from './TeacherRegisterApp';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorldApp,
+  TeacherRegisterApp,
 });
