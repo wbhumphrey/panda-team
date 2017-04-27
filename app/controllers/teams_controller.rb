805fe795afbb2cc089045616ca8b73fd19e2e0e0
Brad Humphrey <brad@instructure.com>
2017-04-27 16:52:40 -0700

    merge calendar events and assignments in chronological order

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -18,16 +18,37 @@ class TeamsController < ApplicationController
 
   def index
     query_params = {
+      per_page: 1000,
       start_date: 1.week.ago.iso8601,
       end_date: 3.weeks.from_now.iso8601,
-      context_codes: ["course_#{1}"]
+      context_codes: ["course_#{1}"],
+      type: 'event' #|| 'assignment'
     }
 
     puts query_params.to_query
     response = canvas_api('/api/v1/calendar_events', query_params.to_query)
-    # events = JSON.parse(response.body)
+    events = JSON.parse(response.body)
 
-    render plain: response.body
+    response = canvas_api('/api/v1/calendar_events', query_params.merge(type: 'assignment').to_query)
+    assignments = JSON.parse(response.body)
+
+    calendar_items = []
+    loop do
+      if events.empty?
+        calendar_items.push(*assignments)
+        break
+      elsif assignments.empty?
+        calendar_items.push(*events)
+        break
+      end
+
+      event_start = DateTime.parse(events.first['start_at'])
+      assignment_start = DateTime.parse(assignments.first['start_at'])
+
+      calendar_items << (event_start < assignment_start ? events.shift : assignments.shift)
+    end
+
+    render plain: calendar_items.to_json
   end
 
   private
